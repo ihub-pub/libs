8651494443631d61323e9ac7213f41778d60c574
henry <henry.git@outlook.com>
2021-09-19 21:05:30 +0800

    ✨ 添加test、swagger、cloud组件 (#18)
    * ✨ 添加test、swagger、cloud组件
    
    * 🎨 自动生成lombok.config

---
+++ ihub-starter/test/src/main/java/pub/ihub/test/IHubFTConfig.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2021 Henry 李恒 (henry.box@outlook.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package pub.ihub.test;
+
+import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * IHub测试注解
+ *
+ * @author henry
+ */
+@SpringBootTest
+@ContextConfiguration(
+	classes = IHubTestConfiguration.class,
+	initializers = ConfigDataApplicationContextInitializer.class
+)
+@Retention(RUNTIME)
+@Target(TYPE)
+public @interface IHubFTConfig {
+
+}
