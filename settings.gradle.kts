80215404409e25bf49b2198d4b8c40765b1234d9
henry <henry.git@outlook.com>
2023-08-13 13:37:59 +0800

    feat(java): 添加Java11组件版本配置
    Signed-off-by: henry <henry.git@outlook.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2021 Henry 李恒 (henry.box@outlook.com).
+ * Copyright (c) 2021-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,11 +16,10 @@
 import pub.ihub.plugin.IHubSettingsExtension
 
 plugins {
-    id("pub.ihub.plugin.ihub-settings") version "1.3.5"
+    id("pub.ihub.plugin.ihub-settings") version "1.4.0-m1"
 }
 
 configure<IHubSettingsExtension> {
-    includeProjects("ihub-dependencies")
     includeProjects("ihub-core", "ihub-process").skippedDirs("doc").subproject
     includeProjects("ihub-starter").prefix("ihub-boot-").suffix("-spring-boot-starter").onlySubproject
 //    includeProjects "ihub-sso" subproject "-spring-boot-starter"
