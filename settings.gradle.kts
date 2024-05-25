8d6453ad2cd75f6843b799b73deb47a489ebeb98
Henry <henry.git@outlook.com>
2024-05-25 13:32:25 +0800

    更新 settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,7 +16,7 @@
 import pub.ihub.plugin.IHubSettingsExtension
 
 plugins {
-    id("pub.ihub.plugin.ihub-settings") version "1.6.3-rc1"
+    id("pub.ihub.plugin.ihub-settings") version "1.6.3-rc2"
 }
 
 configure<IHubSettingsExtension> {
